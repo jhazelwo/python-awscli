4e32565ac6e52b358ac7ba7d0469dd21acc46f1b
John Hazelwood <jhazelwo@users.noreply.github.com>
2016-12-29 14:05:20 -0700

    Thu Dec 29 14:05:20 MST 2016

--- python2awscli/model/efs.py
+++ python2awscli/model/efs.py
@@ -1,18 +1,35 @@
-#!/usr/bin/env python3
 """ -*- coding: utf-8 -*- """
 import uuid
+from pprint import pprint
+
+from python2awscli import bin_aws
 
 
 class BaseEFS(object):
-    def __init__(self, name, region, kind, zones):
+    def __init__(self, name, region, kind, zones, subnet, mount=None):
         self.name = name
         self.token = uuid.uuid5(uuid.NAMESPACE_URL, self.name)
         self.region = region
         self.kind = kind
         self.zones = zones
+        self.subnet = subnet
+        self.mount = mount
+        if not mount:
+            self.mount = name
 
     def _create(self):
-        pass
+        command = ['efs', 'create-file-system', '--region', self.region,
+                   ]
+        result = bin_aws(command)
+        pprint(result)
+        command = ['efs', 'create-mount-target', '--region', self.region,
+                   ]
+        result = bin_aws(command)
+        pprint(result)
 
     def _get(self):
-        pass
+        command = ['efs', 'describe-file-systems', '--region', self.region,
+                   ]
+        result = bin_aws(command)['FileSystems']
+        pprint(result)
+
