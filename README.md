da6795e1aa14855efb1e50fa964b6d01e45472d3
John Hazelwood <jhazelwo@users.noreply.github.com>
2016-12-31 16:54:05 -0700

    Sat Dec 31 16:54:05 MST 2016

--- README.md
+++ README.md
@@ -6,5 +6,37 @@ container
 can run this code already has full access to the bin/aws program. We
 let AWS's code and permission structure do the real work.
 
+## Usage
+* Full examples in __doc/example.py__
+* Some brief examples:
+
+```
+vpc_dev = VPC(name='DEV', region='us-east-1', cidr='10.10.0.0/16', ipv6=False)
+
+my_subnet_a = Subnet(name='www-a', region='us-east-1', vpc=vpc_dev.id, cidr='10.10.1.0/24', zone='us-east-1a')
+
+web_servers_key_pair = KeyPair('/aws/private', 'webservers', 'us-east-1')
+
+web_servers = Instance(
+    name='webservers',
+    region='us-east-1',
+    vpc=vpc_dev.id,
+    image='ami-b7a114d7',  # Ubuntu Server 16.04 LTS (HVM), SSD Volume Type
+    key=web_servers_key_pair.name,
+    count=1,
+    size='t2.micro',
+    groups=web_server_security_group.id,
+    public=True,
+    script='/aws/private/webserver-post-install.sh'
+)
+
+for this in web_servers.id:
+    each = Volume(
+        name='webserver-data', region=REGION, zone=web_servers.zone,
+        kind='gp2', size='1000', instance=this, device='/dev/xvdd'
+    )
+```
+
 ## TODO
-* pass
+* Casual code clean-ups
+* Waiting for new use case
--- doc/example.py
+++ doc/example.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """ -*- coding: utf-8 -*-
-This example OK as of cea59e92a2e53167715e2516cb6583cec4e7e12e
-"""
-exit()
+OK as of 2cdca9337110aa69ebdf43be8926ba42286b4640
 
+This code will:
+    Set up a new VPC with 3 subnets.
+    Create a NFS (EFS) mount available in those subnets
+    Create a web load balancer, keypair, 1 running instance
+    Create a small pgSQL RDS instance
+    Create security groups for all resources to allow correct public, private and neighbor access.
+    Create and attach a 1000gb data volume to the webserver instance.
+
+All methods check for existing resources before creation to prevent duplicates.
+
+No methods currently exist that stop, destroy, delete or decommission resources.
+"""
 from python2awscli.model import Instance, LoadBalancer, VPC, SecurityGroup, KeyPair, RDS, Volume, Subnet, EFS
 from python2awscli.task import make_ipperms, make_listener
 
@@ -16,10 +26,9 @@ EGRESS = {'Ipv6Ranges': [], 'PrefixListIds': [], 'IpRanges': [{'CidrIp': '0.0.0.
 
 vpc_dev = VPC(name='DEV', region=REGION, cidr='10.10.0.0/16', ipv6=False)
 
-my_subnet_a = Subnet(name='app-a', region=REGION, vpc=vpc_dev.id, cidr='10.10.1.0/24', zone='us-west-2a')
-my_subnet_b = Subnet(name='app-b', region=REGION, vpc=vpc_dev.id, cidr='10.10.2.0/24', zone='us-west-2b')
-my_subnet_c = Subnet(name='app-c', region=REGION, vpc=vpc_dev.id, cidr='10.10.3.0/24', zone='us-west-2c')
-
+my_subnet_a = Subnet(name='app-a', region=REGION, vpc=vpc_dev.id, cidr='10.10.1.0/24', zone='us-east-1a')
+my_subnet_b = Subnet(name='app-b', region=REGION, vpc=vpc_dev.id, cidr='10.10.2.0/24', zone='us-east-1b')
+my_subnet_c = Subnet(name='app-c', region=REGION, vpc=vpc_dev.id, cidr='10.10.3.0/24', zone='us-east-1c')
 
 sg_nfs = SecurityGroup(name='nfs', region=REGION, vpc=vpc_dev.id,
                        description='NFS',
@@ -30,16 +39,10 @@ mount = EFS(name='appservers:/app/etc', region=REGION, kind='maxIO',
             subnets=[my_subnet_a.id, my_subnet_b.id, my_subnet_c.id],
             groups=sg_nfs.id)
 
-vpc_prod = VPC(
-    name='PRODUCTION',
-    region=REGION,
-    cidr='10.4.0.0/16',
-    ipv6=False,
-)
 www_lb_sg = SecurityGroup(
     name='www',
     region=REGION,
-    vpc=vpc_prod.id,
+    vpc=vpc_dev.id,
     description='Web Load Balancers',
     inbound=[
         make_ipperms('0.0.0.0/0:80/tcp'),
@@ -49,11 +52,12 @@ www_lb_sg = SecurityGroup(
 )
 
 TIER = 'webservers'
+# Create
 web_servers_key_pair = KeyPair('/aws/private', TIER, REGION)
 web_server_security_group = SecurityGroup(
     name=TIER,
     region=REGION,
-    vpc=vpc_prod.id,
+    vpc=vpc_dev.id,
     description='Web Server Instances',
     inbound=[
         make_ipperms('{0}/9876543210:80/tcp'.format(www_lb_sg.id)),  # LB
@@ -61,10 +65,11 @@ web_server_security_group = SecurityGroup(
     ],
     outbound=EGRESS
 )
+# Create {count} number of instances
 web_servers = Instance(
     name=TIER,
     region=REGION,
-    vpc=vpc_prod.id,
+    vpc=vpc_dev.id,
     image='ami-b7a114d7',  # Ubuntu Server 16.04 LTS (HVM), SSD Volume Type
     key=web_servers_key_pair.name,
     count=1,
@@ -73,6 +78,13 @@ web_servers = Instance(
     public=True,
     script='/aws/private/webserver-post-install.sh'
 )
+# Create and attach a 1000gb EBS volume for each instance in web_servers
+for this in web_servers.id:
+    each = Volume(
+        name='webserver-data', region=REGION, zone=web_servers.zone,
+        kind='gp2', size='1000', instance=this, device='/dev/xvdd'
+    )
+
 
 www_lb = LoadBalancer(
     name='www',
@@ -93,14 +105,14 @@ www_lb = LoadBalancer(
 db_sg = SecurityGroup(
     name='db',
     region=REGION,
-    vpc=vpc_prod.id,
+    vpc=vpc_dev.id,
     description='Databases',
     inbound={
         'FromPort': 5432,
         'IpProtocol': 'tcp',
         'IpRanges': [
             {'CidrIp': '8.8.8.8/32'},    # HOME
-            {'CidrIp': '10.4.0.0/16'}],  # PRODUCTION VPC
+            {'CidrIp': '10.10.0.0/16'}],  # DEV VPC
         'Ipv6Ranges': [],
         'PrefixListIds': [],
         'ToPort': 5432,
@@ -120,8 +132,3 @@ my_db = RDS(
     groups=[db_sg.id],
     public=True
 )
-for this in web_servers.id:
-    each = Volume(
-        name='webserver-data', region=REGION, zone=web_servers.zone,
-        kind='gp2', size='1000', instance=this, device='/dev/xvdd'
-    )
